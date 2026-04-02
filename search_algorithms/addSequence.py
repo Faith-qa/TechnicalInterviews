869444993d5cc0abccec27e5cee9ec5c9c5062b7
Faith-qa <atienofaith12@gmail.com>
2026-04-02 11:57:32 +0300

    added search algorithm reading material

--- search_algorithms/addSequence.py
+++ search_algorithms/addSequence.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-
+# resources:
+"""
+video Playlist:- https://savanna.alxafrica.com/rltoken/_4-JUPlg6lfKZO2YPHCA7g
+space complexty: https://savanna.alxafrica.com/rltoken/QK9ENdoTyqGs0d4_M3XE3g
+search algorithm:- https://savanna.alxafrica.com/rltoken/ap2kuRv8qrUMyQ0-MY3EXw
+"""
 def add(n):
     tot = 0
     for i in range(n):
